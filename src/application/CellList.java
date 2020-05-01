40ff691e5951451b6b74fe2dccec1c0e9744360f
mariusraht2 <mariusraht+2@gmail.com>
2020-05-01 17:33:53 +0200

    Added getBy for CellList

--- src/application/CellList.java
+++ src/application/CellList.java
@@ -3,17 +3,20 @@ package application;
 public class CellList {
 	private Cell[][] cells = new Cell[Main.getEnvironment().getWidth()][Main.getEnvironment().getHeight()];
 	
-	public Cell[][] getCells() {
+	public Cell[][] getCells()
+	{
 		return cells;
 	}
 	
-	public void setCells(Cell[][] cells) {
+	public void setCells(Cell[][] cells)
+	{
 		this.cells = cells;
 	}
 	
 	private static CellList instance;
 	
-	public static CellList getInstance() {
+	public static CellList getInstance()
+	{
 		if (instance == null) {
 			instance = new CellList();
 		}
@@ -21,16 +24,26 @@ public class CellList {
 		return instance;
 	}
 	
-	protected CellList() {}
+	protected CellList()
+	{}
 	
-	public Cell getCell(int x, int y) {
+	public Cell getCell(int x, int y)
+	{
 		return cells[x][y];
 	}
 	
-	public Cell getBy(CellType cellType) {
+	public Cell getBy(CellType cellType)
+	{
+		Cell cell = null;
 		
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; j++) {
+				if (cells[i][j].getType().equals(cellType)) {
+					cell = cells[i][j];
+				}
+			}
+		}
 		
-		
-		return ;
+		return cell;
 	}
 }
