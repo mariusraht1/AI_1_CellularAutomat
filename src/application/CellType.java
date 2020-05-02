f8b20a69d15217bd2809864d32c2a568b9a12108
mariusraht2 <mariusraht+2@gmail.com>
2020-05-02 17:20:45 +0200

    Determine NumOfCells by CellType

--- src/application/CellType.java
+++ src/application/CellType.java
@@ -2,12 +2,9 @@ package application;
 
 import javafx.scene.paint.Color;
 
-public enum CellType {
-	DUMMY(Color.WHITE),
-	PREDATOR(Color.RED),
-	PREY(Color.DODGERBLUE),
-	GRASS1(Color.KHAKI),
-	GRASS2(Color.GREENYELLOW),
+public enum CellType
+{
+	DUMMY(Color.WHITE), PREDATOR(Color.RED), PREY(Color.DODGERBLUE), GRASS1(Color.KHAKI), GRASS2(Color.GREENYELLOW),
 	GRASS3(Color.LIMEGREEN);
 	
 	private Color color;
@@ -15,12 +12,29 @@ public enum CellType {
 	private CellType(Color color) {
 		this.setColor(color);
 	}
-
-	public Color getColor() {
+	
+	public Color getColor()
+	{
 		return color;
 	}
-
-	public void setColor(Color color) {
+	
+	public void setColor(Color color)
+	{
 		this.color = color;
+	}
+	
+	public int getNumOfCells()
+	{
+		int numOfCells = 0;
+		
+		for (int i = 0; i < CellList.getInstance().getCells().length; i++) {
+			for (int j = 0; j < CellList.getInstance().getCells()[i].length; j++) {
+				if(CellList.getInstance().getCells()[i][j].getType().equals(this)) {
+					numOfCells++;
+				}
+			}
+		}
+		
+		return numOfCells;
 	}
 }
